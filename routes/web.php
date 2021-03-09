44b298cf002d8bef59b387a00677694a44b6d62c
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-09 17:16:16 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+      'nome' => 'Niccolò',
+      'cognome' => 'Delfino',
+      'collegamenti' => [
+        'chi siamo',
+        'dove trovarci',
+        'contatti',
+        'la nostra mission'
+      ]
+    ];
+    return view('ciao', $data);
 });
